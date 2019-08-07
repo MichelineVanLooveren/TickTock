0d75e31a3bdcfdd2d38640861d8f2c6e965c80ac
MichelineVanLooveren <micheline.vanlooveren@gmail.com>
2019-08-07 13:43:32 +0200

    opstart TickTock

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+        //bootstrap laadt automatisch klasses in ipv elke klasse apart te includen en start vanzelf een sessie
+        // start a session
+        session_start();
+
+        // include all classes
+        spl_autoload_register(function ($class) {
+            require_once __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.$class.'.php';
+        });
