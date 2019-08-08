4486fdb5bcf99a04e3829016be966cc0dc9ad204
MichelineVanLooveren <micheline.vanlooveren@gmail.com>
2019-08-08 09:39:49 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,5 @@
 
         // include all classes
         spl_autoload_register(function ($class) {
-            require_once __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.$class.'.php';
+            require_once __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.$class.'.class.php';
         });
