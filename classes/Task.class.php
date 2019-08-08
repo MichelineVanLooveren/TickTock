536457cf7379660aebed5d3681b4409a149a26f7
MichelineVanLooveren <micheline.vanlooveren@gmail.com>
2019-08-08 09:34:15 +0200

    #3 proces

---
+++ classes/Task.class.php
@@ -0,0 +1,7 @@
+functie voor taak aan te maken 
+functie taak verwijderen 
+taken bepaalde lijst lezen 
+taak completen 
+taak uncompleten
+taak editen 
+get taak by id --> functie om één taak uit te lezen zodat je daar op kan commenten
