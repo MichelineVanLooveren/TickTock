bbd5fb0bcbd00c0a61807ebe7a8ef0e3c06ddb46
MichelineVanLooveren <micheline.vanlooveren@gmail.com>
2019-08-09 11:56:04 +0200

    alles 7/8 + 8/8

--- createList.php
+++ createList.php
@@ -5,7 +5,8 @@ include_once 'includes/head.inc.php';
 <body>
     <form action="" method="post">
         <label for="listName">Maak een nieuwe lijst aan</label>
-        <input type="text" id="listName" name="listName">
+        <input type="text" id="listName" name="listName" placeholder="Geef je lijst een naam"> 
+        <input type="submit" value="Nieuwe lijst aanmaken">
     </form>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php
+include_once 'includes/head.inc.php';
+?>
+<body>
+    <form action="" method="post">
+        <label for="listName">Maak een nieuwe lijst aan</label>
+        <input type="text" id="listName" name="listName">
+    </form>
+</body>
+</html>
