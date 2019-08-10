46fb3e1b6c0dc925a7beb7923beebdd87708110f
MichelineVanLooveren <micheline.vanlooveren@gmail.com>
2019-08-10 10:06:31 +0200

    Update Task.class.php

--- classes/Task.class.php
+++ classes/Task.class.php
@@ -1,7 +1,9 @@
-functie voor taak aan te maken 
-functie taak verwijderen 
-taken bepaalde lijst lezen 
-taak completen 
-taak uncompleten
-taak editen 
-get taak by id --> functie om één taak uit te lezen zodat je daar op kan commenten
+<?php
+
+// functie voor taak aan te maken
+// functie taak verwijderen
+// taken bepaalde lijst lezen
+// taak completen
+// taak uncompleten
+// taak editen
+// get taak by id --> functie om één taak uit te lezen zodat je daar op kan commenten
