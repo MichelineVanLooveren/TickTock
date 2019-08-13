c0e6b54f7abf0bc8c8f792d9347b06fa1e4b15d7
MichelineVanLooveren <49272598+MichelineVanLooveren@users.noreply.github.com>
2019-08-13 11:25:52 +0200

    Add files via upload

---
+++ includes/header.inc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title><?php echo $title; ?></title> <!-- titel per pagina laten veranderen -->
+</head>
+  <body>
+  <main>
+    <header>
+        <h1 class="hidden">TickTock</h1> <!-- layout en SEO-score -->
+        <img src="" alt="">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="admin.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
