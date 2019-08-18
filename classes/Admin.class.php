a0955f95a205313f4cd8d42b7464a4f494d626f7
MichelineVanLooveren <49272598+MichelineVanLooveren@users.noreply.github.com>
2019-08-18 23:40:00 +0200

    update Admin-class 17/8+18/8

--- classes/Admin.class.php
+++ classes/Admin.class.php
@@ -3,7 +3,9 @@
 require_once __DIR__.'/Db.class.php';
 
  class Admin extends DAO
- { //extends voor overerven voor te connecteren met db
+ {
+     // extends voor overerven voor te connecteren met db
+     // functie om alle lijsten te tellen
      public function selectAllLijsten()
      {
          $sql = 'SELECT count(*) as `lijsten` FROM `lijsten`';
@@ -13,7 +15,7 @@ require_once __DIR__.'/Db.class.php';
          return $stmt->fetch(PDO::FETCH_ASSOC); //fetch en niet fetchAll omdat er maar 1 resultaat teruggegeven wordt
      }
 
-     //zowel studenten als admins
+     // functie om zowel studenten als admins te tellen
      public function selectAllUsersTotaal()
      {
          $sql = 'SELECT count(*) as ` userTotaal` FROM `users`';
@@ -23,7 +25,7 @@ require_once __DIR__.'/Db.class.php';
          return $stmt->fetch(PDO::FETCH_ASSOC);
      }
 
-     //studenten
+     // functie om aantal studenten te tellen
      public function selectAllUsers()
      {
          $sql = 'SELECT count(*) as `users` FROM `users` WHERE `isAdmin` = 0';
@@ -33,7 +35,7 @@ require_once __DIR__.'/Db.class.php';
          return $stmt->fetch(PDO::FETCH_ASSOC);
      }
 
-     //admins
+     // functie om aantal admins tellen
      public function selectAllAdmins()
      {
          $sql = 'SELECT count(*) as `admins` FROM `users` WHERE `isAdmin` = 1';
@@ -43,7 +45,7 @@ require_once __DIR__.'/Db.class.php';
          return $stmt->fetch(PDO::FETCH_ASSOC);
      }
 
-     //kijken wat laatste id is, zodat AI altijd eentje meer kan tellen
+     // kijken wat laatste id is, zodat AI altijd eentje meer kan tellen
      public function selectById($id)
      {
          $sql = 'SELECT * FROM `users` WHERE `id` = :id';
@@ -57,24 +59,24 @@ require_once __DIR__.'/Db.class.php';
      // functie om alle info over admins uit te lezen
      public function selectAllAdminInfo()
      {
-         $sql = 'SELECT * FROM `users`';
+         $sql = 'SELECT * FROM `users` WHERE `isAdmin` = 1';
          $stmt = $this->pdo->prepare($sql);
          $stmt->execute();
 
          return $stmt->fetchAll(PDO::FETCH_ASSOC);
      }
 
-     //nieuwe admin aanmaken
+     // functie om nieuwe admin aan te maken
      public function insertadmin($data)
      {
          $errors = $this->validate($data);
          if (empty($errors)) {
-             $sql = 'INSERT INTO `users` (`user_voornaam`, `user_achternaam`, `email`, `passwoord`, `isAdmin`) VALUES (:user_voornaam, :user_achternaam, :email, :passwoord, :isAdmin)';
+             $sql = 'INSERT INTO `users` (`user_voornaam`, `user_achternaam`, `email`, `paswoord`, `isAdmin`) VALUES (:user_voornaam, :user_achternaam, :email, :paswoord, :isAdmin)';
              $stmt = $this->pdo->prepare($sql);
              $stmt->bindValue(':user_voornaam', $data['user_voornaam']);
              $stmt->bindValue(':user_achternaam', $data['user_achternaam']);
              $stmt->bindValue(':email', $data['email']);
-             $stmt->bindValue(':passwoord', $data['passwoord']);
+             $stmt->bindValue(':paswoord', $data['paswoord']);
              $stmt->bindValue(':isAdmin', $data['isAdmin']);
              if ($stmt->execute()) {
                  return $this->selectById($this->pdo->lastInsertId()); //kijken naar laatste id
@@ -84,7 +86,7 @@ require_once __DIR__.'/Db.class.php';
          return false;
      }
 
-     //admin verwijderen
+     // functie om admin te verwijderen
      public function delete($id)
      {
          $sql = 'DELETE FROM `users` WHERE `id` = :id';
@@ -94,7 +96,7 @@ require_once __DIR__.'/Db.class.php';
          return $stmt->execute();
      }
 
-     //foutboodschappen bij aanmaak admin
+     // foutboodschappen bij aanmaak admin
      public function validate($data)
      {
          $errors = [];
@@ -107,8 +109,8 @@ require_once __DIR__.'/Db.class.php';
          if (empty($data['email'])) {
              $errors['email'] = 'Gelieve email in te vullen';
          }
-         if (empty($data['passwoord'])) {
-             $errors['passwoord'] = 'Gelieve passwoord in te vullen';
+         if (empty($data['paswoord'])) {
+             $errors['paswoord'] = 'Gelieve paswoord in te vullen';
          }
 
          return $errors;
